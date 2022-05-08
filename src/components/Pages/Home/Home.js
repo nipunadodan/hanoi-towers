bddec239621eae55ee581c0ab1deae87d528221c
Nipuna Dodantenna <nipunadodan@gmail.com>
2022-05-09 03:16:14 +0530

    UI changed to dark neon

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -155,14 +155,14 @@ class Home extends Component{
         return (
             <>
                 <h1 className={'text-center'}>Hanoi Towers</h1>
-                <p className={'text-center'} style={{color:'#888'}}>Instructions: Select a disc you want to move and then select the Tower name you want your selected disc to move.</p>
+                <p className={'text-center'}>Instructions: Select a disc you want to move and then select the Tower name you want your selected disc to move.</p>
                 <div className={'text-center'} style={{padding:'10px'}}>
                     Discs count
                     <span onClick={this.changeDisksCount('dn')} onMouseDown={this.avoidSelect} className={'button'}>-</span>
-                    <span style={{fontSize:'1.5em', 'margin':'0 5px'}}>{this.state.noOfDisks}</span>
+                    <span style={{fontSize:'1.5em', 'margin':'0 10px',fontWeight:'bold'}}>{this.state.noOfDisks}</span>
                     <span onClick={this.changeDisksCount('up')} onMouseDown={this.avoidSelect} className={'button'}>+</span>
                     <span onClick={this.reset} onMouseDown={this.avoidSelect} style={{color: '#888', margin:'10px', cursor:'pointer'}}>Reset</span>
-                    <div style={{margin:'15px 0 10px'}}> The challenge is to finish the game in a minimum moves of {2**this.state.noOfDisks - 1}</div>
+                    <div style={{margin:'15px 0 10px'}}>Complete the game in a minimum moves of {2**this.state.noOfDisks - 1}</div>
                 </div>
                 <div className={'towers'}>
                     {
@@ -179,7 +179,9 @@ class Home extends Component{
                                                 className={'disk'}
                                                 style={{
                                                     width: (x / this.state.noOfDisks * 90) + '%',
-                                                    background: (activeDisk.disk === x ? '#f00' : '#555')
+                                                    background: (activeDisk.disk === x ? '#34C1FE' : '#34C1FE55'),
+                                                    color: (activeDisk.disk === x ? '#fff' : '#80D9FF'),
+                                                    borderColor: (activeDisk.disk === x ? '#34C1FE' : '#80D9FF'),
                                                 }}
                                             >{x}</div>
                                         )
@@ -191,7 +193,7 @@ class Home extends Component{
                 </div>
 
                 <div className={'text-center'} style={{margin:'20px 0'}}>
-                    Moves by you <span style={{background:(this.state.moves <= 2**this.state.noOfDisks - 1 ? 'green' : 'red'), padding: '10px', color:'white', borderRadius: '5px', width:'20px', display:'inline-block'}}>{this.state.moves}</span> Timer: {this.state.timeLapsed} s
+                    Moves by you <span style={{background:(this.state.moves <= 2**this.state.noOfDisks - 1 ? 'green' : 'red'), padding: '10px', color:'white', borderRadius: '5px', width:'20px', display:'inline-block',margin:'0 35px 0 5px'}}>{this.state.moves}</span> Timer: {this.state.timeLapsed} s
                     {
                         this.state.won && this.state.moves === 2**this.state.noOfDisks - 1
                             ? <p key={'won'} style={{color: 'green'}}>You won!</p>
